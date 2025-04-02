4e5a374cfd439ea2f23dac9f91b0be0c97d935a4
diplodocuslongus <hinoki.kuaimu@gmail.com>
2025-04-02 14:16:58 +0800

    more cleanup, standalone animation with matplotlib

--- lib/robot_standalone_.py
+++ lib/robot_standalone_.py
@@ -77,13 +77,15 @@ class Robot:
         self.update_velocity()
         self.update_location() # position: t => t+1
 
-    def loop(self, num_steps, draw=True, anim_plot=True):
-        if draw:
-            fig, (simulation_plot, polar_plot, histogram_grid_plot) = plt.subplots(1, 3, figsize=(18, 6))
+    def loop(self, num_steps, draw=True):
+        plt.ion() # enable interactive plotting mode
+        if draw == True:
+            figure, (simulation_plot, polar_plot, histogram_grid_plot) = plt.subplots(1, 3, figsize=(18, 6))
+            # time.sleep(1)
 
-            # Initialize simulation plot
-            obstacles_x, obstacles_y = self.path_planner.histogram_grid.get_obstacles()
-            paths_robot, = simulation_plot.plot(*self.location, 'bo')
+            # 1. Plot the simulation
+            obstacles_x, obstacles_y = self.path_planner.histogram_grid.get_obstacles() # get a list of points [(x1, y1), (x2, y2), ...]
+            paths_robot = simulation_plot.scatter(*self.location, color='blue')
             paths_target = simulation_plot.scatter(*self.path_planner.target_location, color='green')
             paths_obstacles = simulation_plot.scatter(obstacles_x, obstacles_y, color='red')
             active_region_min_x, active_region_min_y, active_region_max_x, active_region_max_y = self.path_planner.histogram_grid.get_active_region(self.location)
@@ -97,71 +99,102 @@ class Robot:
             )
             simulation_plot.invert_yaxis()
 
-            # Initialize polar histogram plot
+            # 2. Plot the polar histogram
             num_bins = self.path_planner.polar_histogram.num_bins
             valley_threshold = self.path_planner.valley_threshold
             polar_histogram_by_angle = self.path_planner.polar_histogram.get_angle_certainty()
-            bin_percentages = [1.0 / num_bins for angle, certainty in polar_histogram_by_angle]
+            # NOTE: instead of sectors, get polar histogram bins and filter them by valley threshold
+            bin_percentages = [1.0/num_bins for angle, certainty in polar_histogram_by_angle]
             bin_certainties = [certainty for angle, certainty in polar_histogram_by_angle]
             colors = ['blue' if certainty < valley_threshold else 'red' for angle, certainty in polar_histogram_by_angle]
             labels = [angle for angle, certainty in polar_histogram_by_angle]
-            # generator = enumerate(polar_histogram_by_angle)
-            # trick here: we create a new generator (local_generator) for each call. 
-            # Ensures that the generator is reset for every frame of the animation
-            # otherwise we have a 'StopIteration' error
+            generator = enumerate(polar_histogram_by_angle)
             def make_autopct(bin_percentages):
-                local_generator = enumerate(polar_histogram_by_angle) # Recreate the generator
                 def my_autopct(pct):
-                    index, (angle, certainty) = next(local_generator)
-                    return '{certainty:.1f}'.format(certainty=certainty)
+                    index, (angle, certainty) = next(generator)
+                    return '{angle:.0f}:  {certainty:.1f}'.format(angle=angle, certainty=certainty)
                 return my_autopct
 
+            pie_patches, pie_texts, pie_autotexts = polar_plot.pie(bin_percentages, colors=colors, labels=labels, startangle=0, counterclock=True, autopct=make_autopct(bin_percentages))
 
-            pie_patches, pie_texts, pie_autotexts = polar_plot.pie(bin_percentages, colors=colors, labels=labels, startangle=0, autopct=make_autopct(bin_percentages))
 
-            # Initialize histogram grid plot
+            # 3. Plot the valley
             histogram_grid_active_region = self.path_planner.histogram_grid.get_histogram_grid_active_region(active_region_min_x, active_region_min_y, active_region_max_x, active_region_max_y)
+            # print('active region histogram =')
+            # print(*histogram_grid_active_region, sep='\n')
+            histogram_grid_plot.clear()
             histogram_grid_plot.matshow(histogram_grid_active_region, origin="upper")
+            print('first plot')
+            # plt.show(block=True)
+            plt.show(block=False)
+            # plt.show()
+            time.sleep(1)
 
-            def animate(i):
-                self.step()
 
-                # Update simulation plot
+        for i in range(num_steps):
+
+            self.step()
+            if draw == True:
+                # plt.gcf()
+                # plt.gca()
+                # figure.clf()
+                # time.sleep(1)
+                # plt.cla()
+                # plt.clf()
+
+                # 1. Replot the simulation
                 obstacles_x, obstacles_y = self.path_planner.histogram_grid.get_obstacles()
-                x_coords = [point[0] for point in [self.location]]
-                y_coords = [point[1] for point in [self.location]]
-
-                paths_robot.set_data(x_coords, y_coords)
-                # paths_robot.set_data(self.location[0], self.location[1])
+                paths_robot = simulation_plot.scatter(*self.location, color='blue')
+                paths_target = simulation_plot.scatter(*self.path_planner.target_location, color='green')
+                paths_obstacles = simulation_plot.scatter(obstacles_x, obstacles_y, color='red')
                 active_region_min_x, active_region_min_y, active_region_max_x, active_region_max_y = self.path_planner.histogram_grid.get_active_region(self.location)
                 rectangle.set_bounds(active_region_min_x, active_region_min_y, active_region_max_x - active_region_min_x, active_region_max_y - active_region_min_y)
 
-                # Update polar histogram plot
+
+                # 2. Replot the polar histogram
+                # sectors = self.path_planner.get_sectors() # NOTE: sectors are only valid
                 num_bins = self.path_planner.polar_histogram.num_bins
                 valley_threshold = self.path_planner.valley_threshold
                 polar_histogram_by_angle = self.path_planner.polar_histogram.get_angle_certainty()
-                bin_percentages = [1.0 / num_bins for angle, certainty in polar_histogram_by_angle]
+                # NOTE: instead of sectors, get polar histogram bins and filter them by valley threshold
+                bin_percentages = [1.0/num_bins for angle, certainty in polar_histogram_by_angle]
                 bin_certainties = [certainty for angle, certainty in polar_histogram_by_angle]
                 colors = ['blue' if certainty < valley_threshold else 'red' for angle, certainty in polar_histogram_by_angle]
                 labels = [angle for angle, certainty in polar_histogram_by_angle]
                 generator = enumerate(polar_histogram_by_angle)
+                def make_autopct(bin_percentages):
+                    def my_autopct(pct):
+                        index, (angle, certainty) = next(generator)
+                        return '{certainty:.1f}'.format(certainty=certainty)
+                    return my_autopct
 
                 polar_plot.clear()
+                # polar_plot.pie(bin_percentages, colors=colors, labels=labels, startangle=0, counterclock=True, autopct=make_autopct(bin_percentages))
                 polar_plot.pie(bin_percentages, colors=colors, labels=labels, startangle=0, autopct=make_autopct(bin_percentages))
 
-                # Update histogram grid plot
+
+                # 3. Replot the histogram_grid
                 histogram_grid_active_region = self.path_planner.histogram_grid.get_histogram_grid_active_region(active_region_min_x, active_region_min_y, active_region_max_x, active_region_max_y)
+                # print('active region histogram =')
+                # print(*histogram_grid_active_region, sep='\n')
                 histogram_grid_plot.clear()
                 histogram_grid_plot.matshow(histogram_grid_active_region, origin="upper")
 
-                return paths_robot, rectangle, pie_patches, histogram_grid_plot
 
-            ani = animation.FuncAnimation(fig, animate, frames=num_steps, interval=200, blit=False) # Adjust interval as needed
-            plt.show()
+                # 4. Actually display the plots
+                # plt.gcf()
+                # plt.pause(0.1)
+                # plt.show()
+                # figure.show() 
+                # figure.canvas.draw()
+                # plt.show(block=True)
 
-        else:
-            for i in range(num_steps):
-                self.step()
+
+
+
+                time.sleep(0.21)
+                # display.display(plt.gcf())
+
 
     def print_histogram(self):
         self.path_planner.print_histogram()
